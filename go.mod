65f28ff0fd35830f77caab1a455d0c0f331e0621
algo7 <11154774+algo7@users.noreply.github.com>
2024-01-17 17:30:30 +0100

    Add Outlook integration to fetch emails

--- go.mod
+++ go.mod
@@ -2,18 +2,44 @@ module github.com/algo7/day-planner-gpt-data-portal
 
 go 1.21.4
 
-require github.com/gofiber/fiber/v2 v2.52.0
+require (
+	github.com/gofiber/fiber/v2 v2.52.0
+	github.com/microsoft/kiota-abstractions-go v1.5.4
+	github.com/microsoftgraph/msgraph-sdk-go v1.30.0
+	github.com/microsoftgraph/msgraph-sdk-go-core v1.0.1
+)
 
 require (
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.9.1 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.5.1 // indirect
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/cjlapao/common-go v0.0.39 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/microsoft/kiota-authentication-azure-go v1.0.1 // indirect
+	github.com/microsoft/kiota-http-go v1.1.1 // indirect
+	github.com/microsoft/kiota-serialization-form-go v1.0.0 // indirect
+	github.com/microsoft/kiota-serialization-json-go v1.0.5 // indirect
+	github.com/microsoft/kiota-serialization-multipart-go v1.0.0 // indirect
+	github.com/microsoft/kiota-serialization-text-go v1.0.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/std-uritemplate/std-uritemplate/go v0.0.50 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	go.opentelemetry.io/otel v1.21.0 // indirect
+	go.opentelemetry.io/otel/metric v1.21.0 // indirect
+	go.opentelemetry.io/otel/trace v1.21.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/integrations/outlook/emails.go
+++ pkg/integrations/outlook/emails.go
@@ -1,47 +1,100 @@
 package outlook
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
 	"log"
-	"net/http"
+	"time"
+
+	abstractions "github.com/microsoft/kiota-abstractions-go"
+	msgraphsdk "github.com/microsoftgraph/msgraph-sdk-go"
+	msgraphcore "github.com/microsoftgraph/msgraph-sdk-go-core"
+	"github.com/microsoftgraph/msgraph-sdk-go/models"
+	graphusers "github.com/microsoftgraph/msgraph-sdk-go/users"
 )
+
+type OutlookEmail struct {
+	Subject          string `json:"subject"`
+	Body             string `json:"body"`
+	Sender           string `json:"sender"`
+	RecievedDateTime string `json:"recievedDateTime"`
+}
+
+// CustomAuthenticationProvider implements the AuthenticationProvider interface
+type CustomAuthenticationProvider struct {
+	accessToken string
+}
+
+// AuthenticateRequest adds the Authorization header to the request
+func (c *CustomAuthenticationProvider) AuthenticateRequest(ctx context.Context, requestInfo *abstractions.RequestInformation, additionalAuthenticationContext map[string]interface{}) error {
+	if requestInfo != nil {
+		requestInfo.Headers.Add("Authorization", "Bearer "+c.accessToken)
+	}
+	return nil
+}
 
 // GetEmails calls the Microsoft Graph API to get the user's emails.
 func GetEmails(accessToken string) error {
-	url := "https://graph.microsoft.com/v1.0/me/messages"
 
-	// Create a new request using http
-	req, err := http.NewRequest("GET", url, nil)
+	// Create an instance of CustomAuthenticationProvider with the access token
+	customAuthProvider := &CustomAuthenticationProvider{accessToken: accessToken}
 
-	// add authorization header to the req
-	req.Header.Add("Authorization", "Bearer "+accessToken)
-
-	// Send req using http Client
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	// Create a new Graph service client with the custom authentication provider
+	adapter, err := msgraphsdk.NewGraphRequestAdapter(customAuthProvider)
 	if err != nil {
-		return fmt.Errorf("failed to create request: %w", err)
-	}
-	defer resp.Body.Close()
-
-	// Parse response JSON
-	var data map[string]interface{}
-
-	err = json.NewDecoder(resp.Body).Decode(&data)
-	if err != nil {
-		log.Printf("Error while decoding the response: %v", err)
-		return err
+		log.Fatalf("Could not create request adapter: %v", err)
 	}
 
-	// Here you can process the data
-	// For now, we just print it
-	// fmt.Println(data)
+	graphClient := msgraphsdk.NewGraphServiceClient(adapter)
 
-	// Loop through the emails and gets the body.content and the subject
-	for _, email := range data["value"].([]interface{}) {
-		fmt.Println(email)
-		// fmt.Println("Body:", email.(map[string]interface{})["bodyPreview"])
+	// Use the graphClient to make API calls
+	user, err := graphClient.Me().Get(context.Background(), nil)
+	if err != nil {
+		log.Fatalf("Error getting user: %v", err)
+	}
+
+	log.Printf("User: %v", user)
+
+	// Get the current time
+	now := time.Now()
+
+	// Subtract 2 days from the current time
+	twoDaysAgo := now.AddDate(0, 0, -2)
+
+	// Format the time in ISO 8601 format
+	twoDaysAgoStr := twoDaysAgo.Format("2006-01-02T15:04:05Z")
+	fmt.Println(twoDaysAgoStr)
+
+	requestFilter := fmt.Sprintf("singleValueExtendedProperties/Any(ep: ep/id eq 'String 0x001A' and contains(ep/value, 'IPM.Note')) and receivedDateTime ge %s ", twoDaysAgoStr)
+
+	requestParameters := &graphusers.ItemMessagesRequestBuilderGetQueryParameters{
+		Select:  []string{"sender", "subject", "receivedDateTime"},
+		Orderby: []string{"receivedDateTime DESC"},
+		Filter:  &requestFilter,
+	}
+	configuration := &graphusers.ItemMessagesRequestBuilderGetRequestConfiguration{
+		QueryParameters: requestParameters,
+	}
+
+	messages, err := graphClient.Me().Messages().Get(context.Background(), configuration)
+
+	if err != nil {
+		return fmt.Errorf("Error getting messages: %w", err)
+	}
+
+	// Initialize iterator
+	pageIterator, _ := msgraphcore.NewPageIterator[*models.Message](messages, graphClient.GetAdapter(), models.CreateMessageCollectionResponseFromDiscriminatorValue)
+
+	// Iterate over all pages
+	err = pageIterator.Iterate(context.Background(), func(message *models.Message) bool {
+		fmt.Printf("%s\n", *message.GetSubject())
+		// Return true to continue the iteration
+		return true
+	})
+
+	// Check for errors
+	if err != nil {
+		return fmt.Errorf("Error iterating over messages: %w", err)
 	}
 
 	return nil
