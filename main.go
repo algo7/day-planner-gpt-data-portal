d7f14df2d62369823fd32c562bbcaebb19fe4721
algo7 <11154774+algo7@users.noreply.github.com>
2024-01-17 02:52:59 +0100

    Update dependencies and configure Fiber app

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import "github.com/gofiber/fiber/v2"
+
 func main() {
+
+	app := fiber.New(fiber.Config{
+		CaseSensitive: true,
+		StrictRouting: true,
+		ServerHeader:  "Day Planner GPT Data Portal",
+		AppName:       "Day Planner GPT Data Portal",
+	})
+
+	app.Listen(":3000")
 
 }
