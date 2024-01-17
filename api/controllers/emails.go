6ada7431a11bb480573ba76c874c90df9ed856a2
algo7 <11154774+algo7@users.noreply.github.com>
2024-01-17 17:49:52 +0100

    Add GetOutlookEmails API endpoint

--- api/controllers/emails.go
+++ api/controllers/emails.go
@@ -1,3 +1,15 @@
 package controllers
 
+import (
+	"github.com/algo7/day-planner-gpt-data-portal/pkg/integrations/outlook"
+	"github.com/gofiber/fiber/v2"
+)
 
+// GetOutlookEmails returns the user's emails.
+func GetOutlookEmails(c *fiber.Ctx) error {
+	emails, err := outlook.GetEmails()
+	if err != nil {
+		return c.SendString(err.Error())
+	}
+	return c.JSON(emails)
+}
--- api/routes/emails.go
+++ api/routes/emails.go
@@ -1,8 +1,11 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/algo7/day-planner-gpt-data-portal/api/controllers"
+	"github.com/gofiber/fiber/v2"
+)
 
 // EmailsRoutes is the route handler for the emails API.
 func EmailsRoutes(app *fiber.App) {
-
+	app.Get("/outlook", controllers.GetOutlookEmails)
 }
