a4cbc876e37ece825c8d105e62ce4608c853bea5
algo7 <11154774+algo7@users.noreply.github.com>
2024-01-18 20:45:35 +0100

    Add API key generation and storage in Redis

--- pkg/utils/apikey.go
+++ pkg/utils/apikey.go
@@ -1,10 +1,15 @@
 package utils
 
 import (
+	"context"
 	"crypto/hmac"
 	"crypto/rand"
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
+	"time"
+
+	redisclient "github.com/algo7/day-planner-gpt-data-portal/internal/redis"
 )
 
 // CreateHMACSigniture creates HMAC signiture for the given string
@@ -22,9 +27,28 @@ func CreateHMACSigniture(secret string, data string) string {
 
 // GenerateAPIKey generates an 32 byte API key in Hex format
 func GenerateAPIKey() (string, error) {
+
+	// Allocate a slice of 32 bytes.
 	bytes := make([]byte, 32)
-	if _, err := rand.Read(bytes); err != nil {
+
+	// Fill the byte slice with cryptographically secure random bytes
+	_, err := rand.Read(bytes)
+	if err != nil {
 		return "", err
 	}
+
+	// Hex encode the bytes
+	apiKey := hex.EncodeToString(bytes)
+
+	// Set the API key in Redis with a TTL of 7 days.
+	ttl := 7 * 24 * time.Hour // 7 days in hours
+
+	// Save the key in the database
+	err = redisclient.Rdb.Set(context.Background(), apiKey, apiKey, ttl).Err()
+	if err != nil {
+		return "", fmt.Errorf("Error saving API key to database: %v", err)
+	}
+
+	// Return the random bytes as a hexadecimal string
 	return hex.EncodeToString(bytes), nil
 }
