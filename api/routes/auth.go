a7f71d160b0e7a2eaef417f76cf5386c99d15ee0
algo7 <11154774+algo7@users.noreply.github.com>
2024-01-18 22:59:39 +0100

    add post and get api key to the route

--- api/routes/auth.go
+++ api/routes/auth.go
@@ -11,4 +11,6 @@ func AuthRoutes(app *fiber.App) {
 	app.Get("/outlook/oauth_redirect", controllers.GetOauthRedirectOutlook).Name("outlook_oauth_redirect")
 	app.Get("/google/auth", controllers.GetAuthGoogle).Name("google_auth")
 	app.Get("/google/oauth_redirect", controllers.GetOauthRedirectGoogle).Name("google_oauth_redirect")
+	app.Get("/apikey", controllers.GetAPIKey).Name("get_api_key")
+	app.Post("/apikey", controllers.PostAPIKey).Name("post_api_key")
 }
