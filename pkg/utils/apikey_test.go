7ee475571dde3a421cfb193c746b8a323e0bec73
algo7 <11154774+algo7@users.noreply.github.com>
2024-01-19 03:34:42 +0100

    add TestGenerateAPIKey

---
+++ pkg/utils/apikey_test.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"testing"
+)
+
+func TestGenerateAPIKey(t *testing.T) {
+	apiKey, err := GenerateAPIKey()
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	expectedLength := 64 // 32 bytes = 64 hexadecimal characters
+	if len(apiKey) != expectedLength {
+		t.Errorf("Expected length: %d, got: %d", expectedLength, len(apiKey))
+	}
+}
