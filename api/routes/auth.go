4703801387f5d1153e10e24b10265dbb9dfa5aa4
algo7 <11154774+algo7@users.noreply.github.com>
2024-01-19 08:57:00 +0100

    update oauth callback url

--- api/routes/auth.go
+++ api/routes/auth.go
@@ -8,9 +8,9 @@ import (
 // AuthRoutes is the route handler for the calendars API.
 func AuthRoutes(app *fiber.App) {
 	app.Get("/v1/auth/oauth/outlook", controllers.GetAuthOutlook).Name("outlook_auth")
-	app.Get("/v1/auth/oauth/outlook/oauth_redirect", controllers.GetOauthRedirectOutlook).Name("outlook_oauth_redirect")
+	app.Get("/v1/auth/oauth/outlook/callback", controllers.GetOauthRedirectOutlook).Name("outlook_oauth_redirect")
 	app.Get("/v1/auth/oauth/google/auth", controllers.GetAuthGoogle).Name("google_auth")
-	app.Get("/v1/auth/oauth/google/oauth_redirect", controllers.GetOauthRedirectGoogle).Name("google_oauth_redirect")
+	app.Get("/v1/auth/oauth/google/callback", controllers.GetOauthRedirectGoogle).Name("google_oauth_redirect")
 	app.Get("/v1/auth/success", controllers.GetAuthSuccess).Name("oauth_success")
 	app.Get("/v1/auth/internal/apikey", controllers.GetAPIKey).Name("get_api_key")
 	app.Post("/v1/auth/internal/apikey", controllers.PostAPIKey).Name("post_api_key")
