aa12b704acacd233b328a47578c29c510b29a414
algo7 <11154774+algo7@users.noreply.github.com>
2024-01-19 04:29:58 +0100

    Remove unused CreateHMACSigniture function

--- pkg/utils/apikey.go
+++ pkg/utils/apikey.go
@@ -1,24 +1,9 @@
 package utils
 
 import (
-	"crypto/hmac"
 	"crypto/rand"
-	"crypto/sha256"
 	"encoding/hex"
 )
-
-// CreateHMACSigniture creates HMAC signiture for the given string
-func CreateHMACSigniture(secret string, data string) string {
-
-	// Create a new HMAC by defining the hash type and the key (as byte array)
-	h := hmac.New(sha256.New, []byte(secret))
-
-	// Write Data to it
-	h.Write([]byte(data))
-
-	// Get result and encode as hexadecimal string
-	return hex.EncodeToString(h.Sum(nil))
-}
 
 // GenerateAPIKey generates an 32 byte API key in Hex format
 func GenerateAPIKey() (string, error) {
@@ -38,3 +23,17 @@ func GenerateAPIKey() (string, error) {
 	// Return the random bytes as a hexadecimal string
 	return apiKey, nil
 }
+
+// Unused
+// // CreateHMACSigniture creates HMAC signiture for the given string
+// func CreateHMACSigniture(secret string, data string) string {
+
+// 	// Create a new HMAC by defining the hash type and the key (as byte array)
+// 	h := hmac.New(sha256.New, []byte(secret))
+
+// 	// Write Data to it
+// 	h.Write([]byte(data))
+
+// 	// Get result and encode as hexadecimal string
+// 	return hex.EncodeToString(h.Sum(nil))
+// }
